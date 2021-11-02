436c872a09a18769c8c47d08eb46e0a5edccda2a
Gaurav Arora <gauravarora.daiict@gmail.com>
2021-11-02 21:28:51 +1100

    Script to add mainnet and testnet socket on local machine.

--- scripts/run_socat.sh
+++ scripts/run_socat.sh
@@ -1,5 +1,8 @@
 dir=$(pwd)
-echo $dir/node.socket
-export CARDANO_NODE_SOCKET_PATH=$dir/node.socket
-socat UNIX-LISTEN:node.socket,fork,reuseaddr,unlink-early, TCP:52.63.196.75:8080&
+echo $dir/testnet/node.socket
+echo $dir/mainnet/node.socket
+export CARDANO_NODE_SOCKET_PATH=$dir/testnet/node.socket
+socat UNIX-LISTEN:$dir/testnet/node.socket,fork,reuseaddr,unlink-early, TCP:52.63.196.75:8080&
+socat UNIX-LISTEN:$dir/mainnet/node.socket,fork,reuseaddr,unlink-early, TCP:52.63.196.75:80&
 disown %1
+disown %2
