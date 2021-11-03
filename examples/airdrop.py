b7efa38895f0dbb7dbfd6aab6d758e204419d8c6
Gaurav Arora <gauravarora.daiict@gmail.com>
2021-11-03 19:37:47 +1100

    Add the part to do airdrop

--- examples/airdrop.py
+++ examples/airdrop.py
@@ -4,11 +4,41 @@ import argparse
 import tqdm
 import json
 import os
+import logging
 
 import pandas as pd
+
 from pydano.blockfrost.top_holder import TopHolders
+from pydano.transaction.transaction import TransactionConfig, BuildTransaction, SignTransaction, SubmitTransaction
 
 parser = argparse.ArgumentParser()
+
+# Argument to interact with blockchain and do drop
+parser.add_argument("--input_address",
+                    help="Address of the input wallet",
+                    type=str,
+                    default=None)
+parser.add_argument("--signing_key",
+                    help="Signing Key",
+                    type=str,
+                    default=None)
+parser.add_argument("--node_socket",
+                    help="Location of node socket to talk to",
+                    type=str,
+                    default='/home/gaurav/Documents/crypto/pydano/node.socket')
+parser.add_argument("--min_utxo",
+                    help="Minting script",
+                    type=int,
+                    default=1324100)
+parser.add_argument("--min_change_utxo",
+                    help="Minting script",
+                    type=int,
+                    default=1775768)
+parser.add_argument("--log_level",
+                     help="Set log level to",
+                     type=str,
+                     default="INFO")
+
 parser.add_argument("--policy_id", help="Policy Id to get top holders for",
                     type=str, default=None)
 parser.add_argument("--api_key", help="Blockfrost API Key",
@@ -25,6 +55,7 @@ parser.add_argument("--sample_airdrop", help='Number of people to sample for air
                     type=int, default=1)
 args = parser.parse_args()
 
+logging.getLogger().setLevel(args.log_level)
 if not args.total_pages:
     raise Exception("Give total_pages")
 
@@ -47,3 +78,34 @@ if args.exclude_address:
     addresses = list(map(lambda x: {'stake': x, 'payment': top_holders.get_payment_address(x)[0]}, addresses))
     print(f"Do airdrop to:", addresses)
 print("Holders found", len(holders))
+
+
+
+##### Going to perform the actual drop
+in_address = args.input_address
+os.environ['CARDANO_NODE_SOCKET_PATH'] = args.node_socket 
+
+tc = TransactionConfig(in_address, args.min_utxo, testnet=not args.mainnet, min_change_utxo=args.min_change_utxo)
+tc.add_input_utxos(in_address)
+all_available_tokens = list(tc.available_tokens)
+
+if len(all_available_tokens) < len(addresses):
+    raise ValueError("Not enough tokens in wallet: {args.in_address}, to do the airdrop")
+
+for idx, address in enumerate(addresses):
+    token_name = all_available_tokens[idx]
+    address['token'] = token_name
+    tc.add_tx_out(address['payment'], token_name, 1)
+
+bt = BuildTransaction(tc, testnet=not args.mainnet)
+bt.run_transaction()
+print("Signing Transaction")
+st = SignTransaction(bt, args.signing_key)
+st.run_transaction()
+print("Submitting Transaction")
+st = SubmitTransaction(st)
+st.run_transaction()
+
+print("AIRDROP DONE")
+print(addresses)
+pd.DataFrame(addresses).to_csv("airdrop.csv")
