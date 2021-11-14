66110e423d836c24e7022ffaea817c35d548cbe4
Gaurav Arora <gauravarora.daiict@gmail.com>
2021-11-14 22:21:56 +1100

    Restructure pydano_cli to match console_script requirements of packaging

--- scripts/pydano_cli.py
+++ scripts/pydano_cli.py
@@ -12,98 +12,7 @@ from pydano.transaction.mint_transaction import MintTransaction, MintRawTransact
 from pydano.transaction.policy_transaction import PolicyIDTransaction
 from pydano.query.utxo import UTXOs
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_address",
-                    help="Address of the input wallet",
-                    type=str,
-                    default=None)
-parser.add_argument("--signing_key",
-                    help="Signing Key",
-                    type=str,
-                    default=None)
-parser.add_argument("--log_level",
-                     help="Set log level to",
-                     type=str,
-                     default="INFO")
-parser.add_argument("--mainnet",
-                     help="Set transaction network to mainnet",
-                     action='store_true')
-parser.add_argument("--node_socket",
-                    help="Location of node socket to talk to",
-                    type=str,
-                    default='/home/gaurav/Documents/crypto/pydano/node.socket')
-parser.add_argument("--min_utxo",
-                    help="Minting script",
-                    type=int,
-                    default=1324100)
-parser.add_argument("--min_change_utxo",
-                    help="Minting script",
-                    type=int,
-                    default=4275768)
-
-parser.add_argument("--minting_script",
-                    help="Minting script",
-                    type=str,
-                    default=None)
-parser.add_argument("--metadata_json",
-                    help="Metadata Json file",
-                    type=str,
-                    default=None)
-parser.add_argument("--pay", help="A json file with list of all payments to be made.",
-                    type=str,
-                    default=None)
-parser.add_argument("--mint", help="A json file with list tokens to mint and address to send them to.",
-                    type=str,
-                    default=None)
-parser.add_argument("--receive_mint", help="enable receive mint.",
-                    action='store_true')
-parser.add_argument("--token_cost",
-                    help="Cost for minting the token",
-                    type=int,
-                    default=2000000)
-parser.add_argument("--transaction_cost",
-                    help="Cost incurred for the network",
-                    type=int,
-                    default=160000)
-parser.add_argument("--receiver_wallet",
-                    help="Address of the receiver wallet",
-                    type=str,
-                    default=None)
-parser.add_argument("--token_name",
-                    help="Token name for receiver mint",
-                    type=str,
-                    default=None)
-
-
-parser.add_argument("--blockfrost_key",
-                    help="Api key of blockfrost",
-                    type=str,
-                    default=None)
-args = parser.parse_args()
-
-logging.getLogger().setLevel(args.log_level)
-
-is_testnet = not args.mainnet
-blockfrost_base_url = ApiUrls.mainnet.value
-if is_testnet:
-    blockfrost_base_url = ApiUrls.testnet.value
-
-blockfrost_api = None
-if args.receive_mint and not args.blockfrost_key:
-    raise ValueError("Need blockfrost_key to do receive_mint")
-if args.receive_mint:
-    blockfrost_api = BlockFrostApi(project_id=args.blockfrost_key, base_url=blockfrost_base_url)
-
-if not args.input_address:
-    raise ValueError("Except to have input_address to do transaction")
-
-if not args.signing_key:
-    raise ValueError("Except to have signing key for input address")
-
-in_address = args.input_address
-os.environ['CARDANO_NODE_SOCKET_PATH'] = args.node_socket 
-
-def do_receive_miint():
+def do_receive_mint(args):
     query_utxo = UTXOs(testnet=not args.mainnet)
     utxos, available_lovelace, available_tokens = query_utxo.utxos(args.input_address)
     for utxo in utxos:
@@ -146,45 +55,144 @@ def do_receive_miint():
             print(e)
             
 
-tc = TransactionConfig(in_address, args.min_utxo, testnet=not args.mainnet, min_change_utxo=args.min_change_utxo)
-if args.pay:
-    print("Doing Pay Transaction")
-    tc.add_input_utxos(in_address)
-    all_payee = json.load(open(args.pay, 'r'))
-    for payee in all_payee:
-        address = payee['address']
-        token_name = payee['token_name']
-        quantity = payee['quantity']
-        tc.add_tx_out(address, token_name, quantity)
-    print("Building Transaction")
-    bt = BuildRawTransaction(tc, testnet=not args.mainnet)
-    refund_adj_fee = AdjustFeeTransaction(bt, tc)
-    bt = refund_adj_fee.run_transaction()
-elif args.mint:
-    print("Doing Mint Transaction")
-    tc.add_input_utxos(in_address)
-    bt = MintRawTransaction(tc, minting_script_file=args.minting_script, metadata_json_file=args.metadata_json, testnet=not args.mainnet)
-    all_payee = json.load(open(args.mint, 'r'))
-    for payee in all_payee:
-        address = payee['address']
-        token_name = payee['token_name']
-        tc.add_mint(address, bt.policyID, token_name)
-    bt.transaction_config = tc
-    refund_adj_fee = AdjustFeeTransaction(bt, tc)
-    bt = refund_adj_fee.run_transaction()
-elif args.receive_mint:
-    while True:
-        try:
-            do_receive_miint()
-        except Exception as e:
-            print(e)
-            pass
-    exit(0)
-                                
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_address",
+                        help="Address of the input wallet",
+                        type=str,
+                        default=None)
+    parser.add_argument("--signing_key",
+                        help="Signing Key",
+                        type=str,
+                        default=None)
+    parser.add_argument("--log_level",
+                         help="Set log level to",
+                         type=str,
+                         default="INFO")
+    parser.add_argument("--mainnet",
+                         help="Set transaction network to mainnet",
+                         action='store_true')
+    parser.add_argument("--node_socket",
+                        help="Location of node socket to talk to",
+                        type=str,
+                        default='/home/gaurav/Documents/crypto/pydano/node.socket')
+    parser.add_argument("--min_utxo",
+                        help="Minting script",
+                        type=int,
+                        default=1324100)
+    parser.add_argument("--min_change_utxo",
+                        help="Minting script",
+                        type=int,
+                        default=4275768)
 
-print("Signing Transaction")
-st = SignTransaction(bt, args.signing_key)
-st.run_transaction()
-print("Submitting Transaction")
-st = SubmitTransaction(st)
-st.run_transaction()
+    parser.add_argument("--minting_script",
+                        help="Minting script",
+                        type=str,
+                        default=None)
+    parser.add_argument("--metadata_json",
+                        help="Metadata Json file",
+                        type=str,
+                        default=None)
+    parser.add_argument("--pay", help="A json file with list of all payments to be made.",
+                        type=str,
+                        default=None)
+    parser.add_argument("--mint", help="A json file with list tokens to mint and address to send them to.",
+                        type=str,
+                        default=None)
+    parser.add_argument("--receive_mint", help="enable receive mint.",
+                        action='store_true')
+    parser.add_argument("--token_cost",
+                        help="Cost for minting the token",
+                        type=int,
+                        default=2000000)
+    parser.add_argument("--transaction_cost",
+                        help="Cost incurred for the network",
+                        type=int,
+                        default=160000)
+    parser.add_argument("--receiver_wallet",
+                        help="Address of the receiver wallet",
+                        type=str,
+                        default=None)
+    parser.add_argument("--token_name",
+                        help="Token name for receiver mint",
+                        type=str,
+                        default=None)
+
+
+    parser.add_argument("--blockfrost_key",
+                        help="Api key of blockfrost",
+                        type=str,
+                        default=None)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    logging.getLogger().setLevel(args.log_level)
+
+    is_testnet = not args.mainnet
+    blockfrost_base_url = ApiUrls.mainnet.value
+    if is_testnet:
+        blockfrost_base_url = ApiUrls.testnet.value
+
+    blockfrost_api = None
+    if args.receive_mint and not args.blockfrost_key:
+        raise ValueError("Need blockfrost_key to do receive_mint")
+    if args.receive_mint:
+        blockfrost_api = BlockFrostApi(project_id=args.blockfrost_key, base_url=blockfrost_base_url)
+
+    if not args.input_address:
+        raise ValueError("Except to have input_address to do transaction")
+
+    if not args.signing_key:
+        raise ValueError("Except to have signing key for input address")
+
+    in_address = args.input_address
+    os.environ['CARDANO_NODE_SOCKET_PATH'] = args.node_socket 
+
+    tc = TransactionConfig(in_address, args.min_utxo, testnet=not args.mainnet, min_change_utxo=args.min_change_utxo)
+    if args.pay:
+        print("Doing Pay Transaction")
+        tc.add_input_utxos(in_address)
+        all_payee = json.load(open(args.pay, 'r'))
+        for payee in all_payee:
+            address = payee['address']
+            token_name = payee['token_name']
+            quantity = payee['quantity']
+            tc.add_tx_out(address, token_name, quantity)
+        print("Building Transaction")
+        bt = BuildRawTransaction(tc, testnet=not args.mainnet)
+        refund_adj_fee = AdjustFeeTransaction(bt, tc)
+        bt = refund_adj_fee.run_transaction()
+    elif args.mint:
+        print("Doing Mint Transaction")
+        tc.add_input_utxos(in_address)
+        bt = MintRawTransaction(tc, minting_script_file=args.minting_script, metadata_json_file=args.metadata_json, testnet=not args.mainnet)
+        all_payee = json.load(open(args.mint, 'r'))
+        for payee in all_payee:
+            address = payee['address']
+            token_name = payee['token_name']
+            tc.add_mint(address, bt.policyID, token_name)
+        bt.transaction_config = tc
+        refund_adj_fee = AdjustFeeTransaction(bt, tc)
+        bt = refund_adj_fee.run_transaction()
+    elif args.receive_mint:
+        while True:
+            try:
+                do_receive_mint(args)
+            except Exception as e:
+                print(e)
+                pass
+        exit(0)
+                                    
+
+    print("Signing Transaction")
+    st = SignTransaction(bt, args.signing_key)
+    st.run_transaction()
+    print("Submitting Transaction")
+    st = SubmitTransaction(st)
+    st.run_transaction()
+
+if __name__ == '__main__':
+    main()
