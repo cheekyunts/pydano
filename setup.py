6ff7fc9408eac4338b9cbf235d999c5841ad3249
Gaurav Arora <gauravarora.daiict@gmail.com>
2021-12-01 21:43:28 +1100

    Remove not required include and exclude

--- setup.py
+++ setup.py
@@ -17,11 +17,7 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
     package_dir={"": "pydano"},
-    packages=setuptools.find_packages(
-        #        exclude=["scripts"],
-        include=["pydano", "pydano.*"],
-        where="pydano",
-    ),
+    packages=setuptools.find_packages(where="pydano"),
     entry_points={"console_scripts": ["pydano-cli=pydano.pydano_cli:main"]},
     install_requires=["blockfrost-python==0.3.1"],
     setup_requires=["pytest-runner", "flake8"],
