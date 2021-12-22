f579175292898f341606502ae69f9546bfc08b6d
Gaurav Arora <gauravarora.daiict@gmail.com>
2021-12-23 00:20:56 +1100

    Change ip to internal vpn ip

--- scripts/run_socat.sh
+++ scripts/run_socat.sh
@@ -4,7 +4,7 @@ mkdir -p $dir/mainet/
 echo $dir/testnet/node.socket
 echo $dir/mainnet/node.socket
 export CARDANO_NODE_SOCKET_PATH=$dir/testnet/node.socket
-socat UNIX-LISTEN:$dir/testnet/node.socket,fork,reuseaddr,unlink-early, TCP:172.31.30.193:8080&
-socat UNIX-LISTEN:$dir/mainnet/node.socket,fork,reuseaddr,unlink-early, TCP:172.31.30.193:80&
+socat UNIX-LISTEN:$dir/testnet/node.socket,fork,reuseaddr,unlink-early, TCP:10.8.0.4:8080&
+socat UNIX-LISTEN:$dir/mainnet/node.socket,fork,reuseaddr,unlink-early, TCP:10.8.0.4:80&
 disown %1
 disown %2
